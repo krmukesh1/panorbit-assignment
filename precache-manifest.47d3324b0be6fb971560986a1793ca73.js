a7cfef187e07d3ec1c44f877bc9612ac4b3a8791
email <krmukeshsingh51@gmail.com>
2020-10-03 18:12:45 +0530

    Updates

---
+++ precache-manifest.47d3324b0be6fb971560986a1793ca73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c69a2cdcfb22d989d4b35dfb2050a08",
+    "url": "/panorbit-assignment/index.html"
+  },
+  {
+    "revision": "e348dfe810396e902226",
+    "url": "/panorbit-assignment/static/css/main.edc64070.chunk.css"
+  },
+  {
+    "revision": "82e794413a43075f6e1c",
+    "url": "/panorbit-assignment/static/js/2.3d127b82.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/panorbit-assignment/static/js/2.3d127b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e348dfe810396e902226",
+    "url": "/panorbit-assignment/static/js/main.fd08b88d.chunk.js"
+  },
+  {
+    "revision": "14ac503d9831a13937e6",
+    "url": "/panorbit-assignment/static/js/runtime-main.e105eaf3.js"
+  }
+]);
